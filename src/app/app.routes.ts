d4a8213f4869847578632ca30c062b286b317f10
Juan Ortega <juanddog99@gmail.com>
2025-10-29 09:16:27 -0600

    paginas y navbar agregados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes Básicos',
+    loadComponent: () => import('./pages/basic-page/basic-page'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes Custom',
+    loadComponent: () => import('./pages/custom-page/custom-page'),
+  },
+  {
+    path: 'default',
+    title: 'Pipes Básicos',
+    loadComponent: () => import('./pages/default-page/default-page'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes Básicos',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  },
+];
---
+++ src/app/pages/default-page/default-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-page',
+  imports: [],
+  templateUrl: './default-page.html',
+})
+export default class DefaultPage {}
